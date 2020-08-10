2db4224733160dacad51ab6e97f17a18e5358766
hiromu0124 <totohiro0124@icloud.com>
2020-08-10 18:48:45 +0900

    変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
-  root "password_forms#new"
+  # root "password_forms#new"
 
-  resources :password_forms 
-  get "app_url", to:"password_forms#new_url"
+  # resources :password_forms 
+  # get "app_url", to:"password_forms#new_url"
 
-  get '*path', to: 'application#render_404'
+  # get '*path', to: 'application#render_404'
   post '/callback' => 'linebot#callback'
 end
